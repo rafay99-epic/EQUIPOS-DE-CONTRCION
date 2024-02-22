230602a01cf2480d1e3f4750292f16aa5072ba54
Abdul Rafay <99marafay@gmail.com>
2024-02-22 18:10:02 +0500

    Code Clean: Ad Confirm Page

--- lib/frontend/views/adconfirm.dart
+++ lib/frontend/views/adconfirm.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class adConfirm extends StatefulWidget {
