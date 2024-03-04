ce4d5ac88b3f6ee66df3f54cd5442d05c083c4f7
Abdul Rafay <99marafay@gmail.com>
2024-03-05 01:24:35 +0500

    Language Support & Some Bug Fixes

--- lib/frontend/widgets/MyDrawer.dart
+++ lib/frontend/widgets/MyDrawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quotaserver/frontend/screens/AdConfirmScreen.dart';
 import 'package:quotaserver/frontend/screens/AdRejectScreen.dart';
